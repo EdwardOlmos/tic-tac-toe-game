ac921f94cfe71252b8c3ee3e14ba4cb40a07e356
Edward Olmos <edwardolmos3@gmail.com>
2021-11-19 10:07:55 -0800

    attach javascript logic to config buttons and name display

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,20 @@
-const playerConfigOverlayElement = document.getElementById('config-overlay');
-const backdropElement = document.getElementById('backdrop');
+let editedPlayer = 0;
+
+const players = [
+  {
+    name: "",
+    symbol: "X",
+  },
+  {
+    name: "",
+    symbol: "O",
+  },
+];
+
+const playerConfigOverlayElement = document.getElementById("config-overlay");
+const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
@@ -7,5 +22,8 @@ const cancelBtnElement = document.getElementById("cancel-config-btn");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
+
 cancelBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
